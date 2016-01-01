5b5d213d4af1b8b0cc16ceecc8c54899b61addb6
Casey Leonard <casey@amphibian.com>
2015-12-31 23:53:24 -0500

    add a dinosaur boss

--- public/js/stage.js
+++ public/js/stage.js
@@ -251,7 +251,81 @@ function stage(gs) {
         };
     
     }
+
+    // any object that should be a dinosaur (boss)
+    function setupDino(game, obj) {
     
+        obj.roarSound = game.sound.add("roar");
+        
+        obj.tailTosser = game.add.emitter(0, 0, 5);
+        obj.tailTosser.makeParticles('dino-plate', 0, 5, true);
+        obj.tailTosser.gravity = 30;
+        obj.tailTosser.maxParticleSpeed.x = -700;
+        obj.tailTosser.minParticleSpeed.x = -400;
+        obj.tailTosser.maxParticleSpeed.y = -500;
+        obj.tailTosser.minParticleSpeed.y = -300;
+        
+        //enemies.add(obj.tailTosser);
+        
+        game.physics.enable(obj, Phaser.Physics.ARCADE);
+        
+        obj.anchor.setTo(0.5, 0);
+        obj.body.collideWorldBounds = true;
+        //obj.body.setSize(60, 25, 0, 38);
+        obj.health = 150;
+        obj.startingX = obj.position.x + 96;
+        obj.walkDir = 1;
+        obj.screaming = false;
+        obj.attacking = false;
+    
+        obj.animations.add("walk", [2, 3], 3, true);
+        obj.animations.play("walk");
+
+        obj.turnAround = function() {
+            this.walkDir = -this.walkDir;
+            this.scream();
+        };
+        
+        obj.scream = function() {
+            this.roarSound.play();
+            this.body.velocity.x = 0;
+            this.screaming = true;
+            this.animations.stop();
+            this.frame = 1;
+            game.time.events.add(1000, function() {
+                this.screaming = false;
+                this.attack();
+            }, this);
+        };
+
+        obj.attack = function() {
+            this.body.velocity.x = 0;
+            this.attacking = true;
+            this.animations.stop();
+            this.frame = 4;
+            this.tailTosser.x = this.position.x + 100;
+            this.tailTosser.y = this.position.y;
+            this.tailTosser.start(true, 2000, null, 5);
+            game.time.events.add(1000, function() {
+                this.attacking = false;
+                obj.animations.play("walk");
+            }, this);
+        };
+
+        obj.update = function() {
+            game.physics.arcade.overlap(frog, this.tailTosser, hurtFrog, null, game);
+            if (this.walkDir == 1 && this.position.x < (this.startingX - 500)) {
+                this.turnAround();
+            } else if (this.walkDir == -1 && this.position.x >= this.startingX) {
+                this.turnAround();
+            }
+            if (!this.screaming && !this.attacking) {
+                this.body.velocity.x = -100 * (this.walkDir);
+            }
+        };
+        
+    }
+
     function setAnimation(f) {
     
         if (f.body.velocity.y === 0) {
@@ -323,12 +397,19 @@ function stage(gs) {
         
         var toads = this.add.group();
         map.createFromObjects('others', 6571, 'toad', 0, true, false, toads);
-        
         toads.forEach(function(t) {
             setupToad(this, t);
         }, this);
         
+        
+        var dinos = this.add.group();
+        map.createFromObjects('others', 6574, 'dinosaur', 0, true, false, dinos);
+        dinos.forEach(function(d) {
+            setupDino(this, d);
+        }, this);
+        
         enemies.addMultiple(toads);
+        enemies.addMultiple(dinos);
         
         map.setCollisionBetween(1, 5);
         map.setCollisionBetween(91, 95);
